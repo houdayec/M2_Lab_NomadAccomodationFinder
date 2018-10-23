50df2901b30f434149bd6a3c43f6b798008e0add
houdayec <houdayer.corentin@gmail.com>
2018-10-23 11:10:33 +0200

    setup spring project

---
+++ src/main/java/com/universita/corsica/nomadaccomodationfinder/NomadaccomodationfinderApplication.java
@@ -0,0 +1,12 @@
+package com.universita.corsica.nomadaccomodationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadaccomodationfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadaccomodationfinderApplication.class, args);
+	}
+}
